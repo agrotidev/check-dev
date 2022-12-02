22cdae696e545e13efbbe9bff65f88f08479d60c
Thiago Lazaro Paraguai <thiago.paraguai@agropeu.com.br>
2022-12-02 07:53:49 -0300

    Criando migration setores

---
+++ database/migrations/2022_12_02_105212_cria_tabela_setores.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('setores', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('departamento');
+
+            $table->integer('cod_setor')->unique();
+            $table->string('nome');
+            $table->boolean('ativo')->default(true);
+
+            $table->foreign('departamento')->references('id')->on('departamentos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('setores', function (Blueprint $table) {
+            //
+        });
+    }
+};
