eeba9966d6ac09dce3d1c579821e665b54c58338
Thiago Lazaro Paraguai <thiago.paraguai@agropeu.com.br>
2022-12-02 08:53:44 -0300

    Adicionando as rotas de admin de recupereção de senha

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,11 @@ Route::prefix('admin')->name('admin.')->group(function () {
         Auth::guard('admin')->logout();
         return redirect()->action([AuthAdminController::class, 'login']);
     })->name('logout');
+
+    // RECOVER PASSWORD ADMIN
+    Route::get('/forgot', [AuthAdminController::class, 'showForgot'])->name('forgot');
+    Route::post('/forgot', [AuthAdminController::class, 'sendForgot'])->name('forgot');
+    Route::get('/reset/{token}', [AuthAdminController::class, 'showResetPassword'])->name('reset.token');
+    Route::post('/reset', [AuthAdminController::class, 'sendResetPassword'])->name('reset');
+
 });
