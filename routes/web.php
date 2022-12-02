70d87fe27726779acfaaab1c601060d4702083f5
Thiago Lazaro Paraguai <thiago.paraguai@agropeu.com.br>
2022-12-02 08:37:36 -0300

    Criando a rotas de login do admin

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,23 @@
 <?php
 
+use App\Http\Controllers\Admin\Auth\AuthAdminController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+// ADMIN
+Route::prefix('admin')->name('admin.')->group(function () {
+
+    // AUTH ADMIN
+    Route::get('/login', [AuthAdminController::class, 'index'])->name('login');
+    Route::post('/login', [AuthAdminController::class, 'login'])->name('login');
+    Route::get('/logout', function () {
+        Auth::guard('admin')->logout();
+        return redirect()->action([AuthAdminController::class, 'login']);
+    })->name('logout');
 });
