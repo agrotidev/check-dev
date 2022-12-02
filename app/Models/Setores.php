09dcde0dbd18ab0c86da1f7f1041646cca66391d
Thiago Lazaro Paraguai <thiago.paraguai@agropeu.com.br>
2022-12-02 07:59:22 -0300

    Criando model Setores

---
+++ app/Models/Setores.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Setores extends Model
+{
+    use HasFactory;
+
+    protected $table = 'setores';
+
+    protected $fillable = ['cod_setor', 'departamento', 'nome', 'ativo'];
+
+    public function departamento()
+    {
+        return $this->hasOne(Departamento::class, 'cod_departamento', 'departamento');
+    }
+
+    public function usuarios()
+    {
+        return $this->hasOne(User::class, 'setor', 'cod_setor');
+    }
+}
