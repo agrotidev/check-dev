3931a8162a742a4e40e873c05ef623ca3f12ee56
Thiago Lazaro Paraguai <thiago.paraguai@agropeu.com.br>
2022-12-02 14:33:47 -0300

    Criando controler SetorController

---
+++ app/Http/Controllers/Admin/SetorController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Setor;
+use Illuminate\Http\Request;
+
+class SetorController extends Controller
+{
+    private $repository;
+
+    public function __construct(Setor $setor)
+    {
+        $this->repository = $setor;
+    }
+
+    public function index()
+    {
+        $setores = $this->repository->latest()->paginate(20);
+    }
+}
