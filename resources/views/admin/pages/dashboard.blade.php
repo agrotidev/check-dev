c3ce047bc027a8b42887e7d520a21f81204951fd
Thiago Lazaro Paraguai <thiago.paraguai@agropeu.com.br>
2022-12-02 16:49:56 -0300

    Criando design de indicadores no dashboard admin

--- resources/views/admin/pages/dashboard.blade.php
+++ resources/views/admin/pages/dashboard.blade.php
@@ -6,6 +6,41 @@
   @include('layouts.components.page-name', ['name' => 'Dashboard Admin'])
 
   <div class="row">
+    <div class="col-md-6 col-lg-3">
+      <div class="widget-small primary coloured-icon"><i class="icon fa fa-users fa-3x"></i>
+        <div class="info">
+          <h4>Usuários</h4>
+          <p><b>5</b></p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6 col-lg-3">
+      <div class="widget-small info coloured-icon"><i class="icon fa fa-thumbs-o-up fa-3x"></i>
+        <div class="info">
+          <h4>Likes</h4>
+          <p><b>25</b></p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6 col-lg-3">
+      <div class="widget-small warning coloured-icon"><i class="icon fa fa-files-o fa-3x"></i>
+        <div class="info">
+          <h4>Uploades</h4>
+          <p><b>10</b></p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6 col-lg-3">
+      <div class="widget-small danger coloured-icon"><i class="icon fa fa-star fa-3x"></i>
+        <div class="info">
+          <h4>Stars</h4>
+          <p><b>500</b></p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
     <div class="col-md-12">
       <div class="tile">
         <div class="tile-body">{{Auth::guard('admin')->user()->name}}</div>
