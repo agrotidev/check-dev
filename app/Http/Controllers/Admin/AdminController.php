f2a3eeb9dd77c3d7b45243643e63a9b97fdf3a10
Thiago Lázaro <thiagolazaro2009@gmail.com>
2022-12-04 20:00:33 -0300

    Adicionando indicadores

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Departamento;
+use App\Models\Setor;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -15,31 +18,39 @@ class AdminController extends Controller
 
     public function index()
     {
-        return view('admin.pages.dashboard');
-    }
+        $usuarios = User::all();
+        $departamentos = Departamento::all();
+        $setores = Setor::all();
 
-    public function login(Request $request)
-    {
-        $request['remember'] = empty($request->remember) ? false : true;
-        $this->validaLogin($request);
-
-        $credenciais = [
-            'email' => $request->email,
-            'password' => $request->password
-        ];
-
-        if (Auth::guard('admin')->attempt($credenciais, $request->remember)) {
-            return redirect()->intended(route('admin.dashboard'));
-        }
-
-        return redirect()->back()->withInputs($request->only('email', 'remember'));
-    }
-
-    protected function validaLogin(Request $request)
-    {
-        $request->validate([
-            'email' => 'required|string',
-            'password' => 'required',
+        return view('admin.pages.dashboard',[
+            'usuarios' => $usuarios,
+            'departamentos' => $departamentos,
+            'setores' => $setores,
         ]);
     }
+
+    // public function login(Request $request)
+    // {
+    //     $request['remember'] = empty($request->remember) ? false : true;
+    //     $this->validaLogin($request);
+
+    //     $credenciais = [
+    //         'email' => $request->email,
+    //         'password' => $request->password
+    //     ];
+
+    //     if (Auth::guard('admin')->attempt($credenciais, $request->remember)) {
+    //         return redirect()->intended(route('admin.dashboard'));
+    //     }
+
+    //     return redirect()->back()->withInputs($request->only('email', 'remember'));
+    // }
+
+    // protected function validaLogin(Request $request)
+    // {
+    //     $request->validate([
+    //         'email' => 'required|string',
+    //         'password' => 'required',
+    //     ]);
+    // }
 }
--- resources/views/admin/pages/dashboard.blade.php
+++ resources/views/admin/pages/dashboard.blade.php
@@ -10,7 +10,7 @@
       <div class="widget-small primary coloured-icon"><i class="icon fa fa-users fa-3x"></i>
         <div class="info">
           <h4>Usuários</h4>
-          <p><b>5</b></p>
+          <p><b>{{ $usuarios->count() }}</b></p>
         </div>
       </div>
     </div>
@@ -23,30 +23,22 @@
       </div>
     </div>
     <div class="col-md-6 col-lg-3">
-      <div class="widget-small warning coloured-icon"><i class="icon fa fa-files-o fa-3x"></i>
+      <div class="widget-small danger coloured-icon"><i class="icon fa fa-building-o"></i>
         <div class="info">
-          <h4>Uploades</h4>
-          <p><b>10</b></p>
+          <h4>Departamentos</h4>
+          <p><b>{{ $departamentos->count() }}</b></p>
         </div>
       </div>
     </div>
     <div class="col-md-6 col-lg-3">
-      <div class="widget-small danger coloured-icon"><i class="icon fa fa-star fa-3x"></i>
+      <div class="widget-small warning coloured-icon"><i class="icon fa fa-files-o fa-3x"></i>
         <div class="info">
-          <h4>Stars</h4>
-          <p><b>500</b></p>
+          <h4>Setores</h4>
+          <p><b>{{ $setores->count() }}</b></p>
         </div>
       </div>
     </div>
   </div>
 
-  <div class="row">
-    <div class="col-md-12">
-      <div class="tile">
-        <div class="tile-body">{{Auth::guard('admin')->user()->name}}</div>
-        <p>{{Auth::guard('admin')->user()->email}}</p>
-      </div>
-    </div>
-  </div>
-    
+
 @endsection
