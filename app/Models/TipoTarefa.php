acae34b18b5e3bbfaaffa8359035aa87c42e7907
Thiago Lazaro Paraguai <thiago.paraguai@agropeu.com.br>
2022-12-09 11:27:47 -0300

    Refatorando model TipoTarefa

--- app/Models/TipoTarefa.php
+++ app/Models/TipoTarefa.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class TipoCategoria extends Model
+class TipoTarefa extends Model
 {
     use HasFactory;
 
