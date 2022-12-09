b6f9c1d57f19b19783d2e1a98e12a4003fb74b86
Thiago Lazaro Paraguai <thiago.paraguai@agropeu.com.br>
2022-12-09 10:23:30 -0300

    Criação de migrations categoria_tarefa | tipo_tarefa | checklist | tarefas

---
+++ database/migrations/2022_12_09_095550_cria_tabela_checklist.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checklists', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('setor');
+            $table->unsignedBigInteger('tipo_tarefas');
+            $table->unsignedBigInteger('user');
+
+
+            $table->string('nome');
+            $table->string('descricao')->nullable();
+            $table->boolean('ativo')->default(true);
+
+            $table->foreign('setor')->references('id')->on('setores');
+            $table->foreign('tipo_tarefas')->references('id')->on('tipo_tarefas');
+            $table->foreign('user')->references('id')->on('users');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checklists');
+    }
+};
---
+++ database/migrations/2022_12_09_100902_cria_tabela_tarefas.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tarefas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('checklist');
+            $table->unsignedBigInteger('categoria_tarefas');
+
+            $table->string('nome');
+            $table->string('descricao')->nullable();
+            $table->float('valor', 8, 2)->default(0);  
+            $table->boolean('ativo')->default(true);
+
+            $table->foreign('checklist')->references('id')->on('checklists');
+            $table->foreign('categoria_tarefas')->references('id')->on('categoria_tarefas');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tarefas');
+    }
+};
