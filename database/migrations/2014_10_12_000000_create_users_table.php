3ba3f4c6a456893404143f9e8b097c7fbf58bac5
Thiago Lazaro Paraguai <thiago.paraguai@agropeu.com.br>
2022-12-23 13:53:00 -0300

    Coluna mobile - users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,6 +24,7 @@ return new class extends Migration
             $table->boolean('active')->default(true);
             $table->boolean('ismanager')->default(false);
             $table->boolean('islider')->default(false);
+            $table->boolean('mobile')->default(false);
 
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
