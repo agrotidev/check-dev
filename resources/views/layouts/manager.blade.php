b17336645f3d9cdd71754eb234d529e97c5b0d37
Thiago Lázaro <thiagolazaro2009@gmail.com>
2023-01-01 17:58:09 -0300

    Refatoração menu checklist

--- resources/views/layouts/manager.blade.php
+++ resources/views/layouts/manager.blade.php
@@ -7,13 +7,18 @@
             <li><a class="dropdown-item" href="#"><i class="fa fa-user fa-lg"></i> Perfil</a></li>
             <li><a class="dropdown-item" href="{{route('manager.logout')}}"><i class="fa fa-sign-out fa-lg"></i> Sair</a></li>
         </ul>
-    </li> 
+    </li>
 @endsection
 
 @section('app-menu')
 <li><a class="app-menu__item  {{ ( Request::routeIs('manager.dash') == Route::current()->getName())  ? 'active' : '' }}" href="{{route('manager.dash')}}"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
 @if ((Auth::user()->islider == true || Auth::user()->ismanager == true))
-<li><a class="app-menu__item {{ ( Request::routeIs('manager.checklist.index') == Route::current()->getName())  ? 'active' : '' }}" href="{{route('manager.checklist.index')}}"><i class="app-menu__icon fa fa-check-square-o"></i><span class="app-menu__label">Checklists</span></a></li>
+<li class="treeview {{ ( Request::routeIs('manager.checklist.*') == Route::current()->getName())  ? 'is-expanded' : '' }}""><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-check-square-o"></i><span class="app-menu__label">Checklist</span><i class="treeview-indicator fa fa-angle-right"></i></a>
+    <ul class="treeview-menu">
+        <li><a class="treeview-item {{ ( Request::routeIs('manager.checklist.index') == Route::current()->getName())  ? 'active' : '' }}" href="{{route('manager.checklist.index')}}"><i class="icon fa fa-check-square-o"></i><span class="app-menu__label">Checklists</span></a></li>
+        <li><a class="treeview-item" href="#"><i class="icon fa fa-object-group"></i><span class="app-menu__label">Grupos</span></a></li>
+    </ul>
+</li>
 @endif
 @endsection
 
